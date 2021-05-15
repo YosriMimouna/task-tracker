e7d1da3f80a72b34e50f3b2488809198cf24e037
YosriMimouna <yosri.mimouna@ensi-uma.tn>
2021-05-15 08:31:36 +0100

    Add MongoConfig class

---
+++ src/main/java/com/blockchaincomputing/tasktracker/configuration/MongoConfig.java
@@ -0,0 +1,36 @@
+package com.blockchaincomputing.tasktracker.configuration;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.database}")
+    private String DB;
+
+    @Value("${spring.data.mongodb.uri}")
+    private String URI;
+
+    @Override
+    protected String getDatabaseName() {
+        return DB;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(URI);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+}
