b4ac4fdee80b0b95d67ca9ab47afe2009fcafaef
YosriMimouna <yosri.mimouna@ensi-uma.tn>
2021-05-15 07:31:20 +0100

    Add unit tests

--- src/test/java/com/blockchaincomputing/tasktracker/TaskTrackerApplicationTests.java
+++ src/test/java/com/blockchaincomputing/tasktracker/TaskTrackerApplicationTests.java
@@ -1,13 +1,40 @@
 package com.blockchaincomputing.tasktracker;
 
+import com.blockchaincomputing.tasktracker.model.LocalTask;
+import com.blockchaincomputing.tasktracker.repository.LocalTaskRepository;
+import com.blockchaincomputing.tasktracker.service.LocalTaskService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class TaskTrackerApplicationTests {
 
+	long currentCount;
+
+	@Autowired
+	LocalTaskRepository localTaskRepository;
+
+	@Autowired
+	LocalTaskService localTaskService;
+
 	@Test
-	void contextLoads() {
+	void testAddLocalTask() {
+		LocalTask newTask = localTaskService.createTask();
+		Optional<LocalTask> addedTask = localTaskRepository.findById(newTask.getId());
+		assert addedTask.isPresent();
 	}
 
+	@Test
+	void testCountIncrement() {
+		this.currentCount = localTaskService.getTaskCount();
+		LocalTask newTask = localTaskService.createTask();
+		this.currentCount++;
+		assertThat(localTaskService.getTaskCount()).isEqualTo(this.currentCount);
+	}
 }
